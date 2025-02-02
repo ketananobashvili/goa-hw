d27e0d7a27f9d23011d9725d3f5934d05d07a9d0
ketananobashvili <liananobashvili1962@icloud.com>
2025-02-02 10:08:37 -0800

    first commit

---
+++ day01/homework/lesson 1.py
@@ -0,0 +1,6 @@
+name = "qeta"
+surname = "nanobashvili"
+age = "16"
+height = "1.58"
+weight = "47"
+print("chemi sakhelia", name, "chemi gvaria", surname, "chemi asakia", age, "chemi simaghlea", height, "chemi wonaa", weight)
