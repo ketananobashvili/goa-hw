f9b2b624be819b98b902a6dca49ff8ec8f2d6c01
ketananobashvili <liananobashvili1962@icloud.com>
2025-03-01 09:29:55 -0800

    commit

---
+++ day03/classwork/cw.py
@@ -0,0 +1,8 @@
+num1 = int(input("enter your number"))
+num2 = int(input("enter your second number"))
+if num1 > num2:
+     print("num1 is more than num2")
+else:
+     print("num2 is more than num1")
+        
+    
