7a652147e6a62deda3e3c708f5bb57a9a396cd91
ketananobashvili <liananobashvili1962@icloud.com>
2025-03-12 11:45:19 -0700

    commit

---
+++ day07/classwork/cw.py
@@ -0,0 +1,12 @@
+#1) შექმენით პროგრამა, რომელიც მომხმარებელს შემოატანინებს 2 რიცხვს, ამ ორ რიცხვს შორის გამოიყენეთ ყველა არითმეტიკული და შედარების ოპერატორები
+
+num1 = int(input("please enter your number: "))
+num2 = int(input("please enter your second number: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 // num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 >= num2)
+print(num1 <= num2)
